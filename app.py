b01d67a8ca3eeb210d9e65b8a8ad683b8c7fb9b2
Robert Smith <robsmith000@hotmail.com>
2022-06-15 15:10:50 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, request
+import json
+
+#run this doc in virtenv 'source virt/scripts/activate
+#to update changes without restarting flask type 'export FLASK_ENV=development', 'export FLASK_APP=app.py'
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+  
+  first_name='Grab first name from database'
+  return render_template("index.html", first_name = first_name)
+
+
+
+
+#error pages
+#invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+  return render_template('404.html'), 404
+  
+
+
+
+@app.errorhandler(500)
+def page_not_found(e):
+  return render_template('500.html'), 500
+
+
+
+
+
+
+
+
+
+
