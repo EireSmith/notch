a6e7738985001218e2427b9a84e1d2746cfa264d
Robert Smith <robsmith000@hotmail.com>
2022-06-15 15:45:16 +0100

    commit

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
 from flask import Flask, render_template, request
+import jinja2
 import json
 
 #run this doc in virtenv 'source virt/scripts/activate
 #to update changes without restarting flask type 'export FLASK_ENV=development', 'export FLASK_APP=app.py'
+
+
+
+jinja_environment = jinja2.Environment(
+  loader=jinja2.FileSystemLoader(['templates', 'templates\error_pages'])
+)
 
 app = Flask(__name__)
 
@@ -20,14 +27,13 @@ def index():
 #invalid URL
 @app.errorhandler(404)
 def page_not_found(e):
-  return render_template('404.html'), 404
-  
+  return render_template("404.html"), 404
 
 
 
 @app.errorhandler(500)
 def page_not_found(e):
-  return render_template('500.html'), 500
+  return render_template("500.html"), 500
 
 
 
