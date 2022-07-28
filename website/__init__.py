c2683c753f107c1e12367e732ce6df3f851031fa
Robert Smith <robsmith000@hotmail.com>
2022-07-28 21:43:28 +0100

    modified:   app.py 	modified:   website/__init__.py

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
-from app import db
 
+db = SQLAlchemy()
 DB_NAME = "notch_database.db"
+
 
 def create_app():
     app = Flask(__name__)
